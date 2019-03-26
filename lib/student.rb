ac61cdf96b0e12e862f57fea79b4728e6e08d819
John Hayes <johnnyhayes7@gmail.com>
2019-03-26 18:49:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -45,8 +45,8 @@ def self.create(name, grade)
 end
 
 def self.new_from_db(row)
-  student = Student.new(row[0], row[1]. row[2])
   binding.pry
+  student = Student.new(row[0], row[1]. row[2])
 end
 
 
